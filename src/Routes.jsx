4f947669137992755b799155c63a35ab4d83a9ce
azharshah95 <38433569+azharshah95@users.noreply.github.com>
2018-07-27 09:21:24 +0500

    BrowserRouter tag moved to Index,js
    Earlier, the BrowserRouter tag was encapsulating Routes in Routes.jsx. Now, I've moved the tag to the Index.js.

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,25 +5,23 @@ import About from "./components/About";
 import Topics from "./components/Topics";
 import Topic from "./components/Topic";
 import ErrorPage from "./components/ErrorPage";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import { Route, Switch } from "react-router-dom";
+//BroswerRouter imported and used in index.js
 class Routes extends Component {
   state = {};
 
   render() {
     return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/login" component={Login} exact />
-            <Route path="/about" component={About} exact />
-            <Route path="/topics" component={Topics} exact />
-            <Route path="/topics/:topicId" component={Topic} exact />
-            <Route component={ErrorPage} exact />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/login" component={Login} exact />
+          <Route path="/about" component={About} exact />
+          <Route path="/topics" component={Topics} exact />
+          <Route path="/topics/:topicId" component={Topic} exact />
+          <Route component={ErrorPage} exact />
+        </Switch>
+      </div>
     );
   }
 }
